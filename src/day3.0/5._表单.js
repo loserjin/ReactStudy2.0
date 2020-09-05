4fdf72360078216313066f77127cb34850825ecd
Loserjin <HJB133144522@163.com>
2020-09-05 11:01:30 +0800

    受控组件和非受控组件

--- src/day3.0/5._表单.js
+++ src/day3.0/5._表单.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component{
+//非受控组件，表单不受组件的数据控制（操作DOM）
+//受控组件，表单受本组件的数据控制，
+class App extends React.Component {
     constructor() {
         super();
     }
-    state={
+
+    state = {
         name: 'baidu'
     }
+
+    componentDidMount() {
+        // this.inp.value = this.state.name//操作DOM
+    }
+
+    change = (e) => {
+        this.setState({
+            name: e.target.value
+        })
+    }
+    fn = () => {
+        console.log(this.state.name)
+    }
+
     render() {
         return <div className=''>
-            <input type="text" value={this.state.name}/>
+            <button onClick={this.fn}>qqq</button>
+            {/*<input type="text" ref={(el)=>{this.inp=el}}/>//操作DOM*/}
+            <input type="text" value={this.state.name} onChange={this.change}/>
+            <p>{this.state.name}</p>
         </div>
     }
 }
-ReactDOM.render(<App/>,document.getElementById('root'))
+
+ReactDOM.render(<App/>, document.getElementById('root'))
